9ee19d3b47780fb956777b1de5cdb969bfa60692
Jovanysfive <jobbento3@gmail.com>
2023-12-26 23:59:03 +0100

    changing de game logic

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ function getComputerChoice(){
    let choise =["rock", "paper", "scissors"]
   return  choise[numrand];
 }
-
+let computerchoise=getComputerChoice();
  function getwinner(playerSelection,computerSelection){
+    /*
     if(playerSelection===computerSelection){
+        console.log("bove are equal")
     }
+    */
     if(
         playerSelection==="rock" && computerSelection==="scissors"||
         playerSelection==="paper" && computerSelection==="rock"||
         playerSelection==="scissors" && computerSelection==="paper"
     ){
         playerscore++;
+        console.log(`Pontuaçaão do jogador: ${playerscore}`)
     }
     if(
         computerSelection==="rock" &&  playerSelection==="scissors"||
@@ -23,6 +27,7 @@ function getComputerChoice(){
         computerSelection==="scissors" &&  playerSelection==="paper"
     ){
         computerscore++;
+        console.log(`Pontuação do computador: ${computerscore}`)
     }
 }
 
@@ -30,14 +35,12 @@ const rock = document.querySelector("#Rock");
 const paper = document.querySelector("#Paper");
 const scissors = document.querySelector("#Scissors");
 
-function handleButtonClick(choice) {
-    console.log(`Clicked ${choice}`);
 
-}
+rock.addEventListener('click', () => getwinner('rock',getComputerChoice()));
+paper.addEventListener('click', () => getwinner('paper',getComputerChoice()));
+scissors.addEventListener('click', () => getwinner('scissors',getComputerChoice()));
 
-rock.addEventListener('click', () => handleButtonClick('Rock'));
-paper.addEventListener('click', () => handleButtonClick('Paper'));
-scissors.addEventListener('click', () => handleButtonClick('Scissors'));
+console.log(getComputerChoice())
 
 
 
